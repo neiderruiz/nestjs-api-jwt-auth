25099d92ad32173674d4bd44cbe9d3fe03c08ed2
Neider Ruiz <57574910+neiderruiz@users.noreply.github.com>
2023-11-01 19:03:31 -0500

    estartegies auth

---
+++ src/auth/strategy/local.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    // username: neiderruiz
+    // password: 123456
+    super({
+      usernameField: 'email',
+      passwordField: 'password',
+    });
+  }
+
+  async validate(email: string, password: string) {
+    const user = await this.authService.validateUser(email, password);
+    console.log(user)
+    if (!user) {
+      throw new UnauthorizedException('Credenciales inválidas');
+    }
+
+    return user;
+  }
+}
